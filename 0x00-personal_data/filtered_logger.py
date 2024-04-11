170b4a72fbc1ac7dc984af6fdf32d1f77b805056
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-11 15:06:52 +0200

    reduce the the number of lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,6 @@ def filter_datum(fields, redaction, message, separator):
     Filter log messages using regex
     """
     for field in fields:
-        pattern = re.compile(
-            rf"({re.escape(separator)}{field}=)[^{separator}]*")
+        pattern = re.compile(rf"({re.escape(separator)}{field}=)[^{separator}]*")
         message = pattern.sub(fr"\1{redaction}", message)
     return message
