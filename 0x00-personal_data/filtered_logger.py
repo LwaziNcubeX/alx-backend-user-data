62205381b4003a637c1aa833d8e0e0d6ec718763
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-11 15:18:47 +0200

    Add type Annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,12 +3,11 @@
 Filtered logger that returns log messages
 """
 import re
+import typing
 
 
-def filter_datum(fields, redaction, message, separator):
-    """
-    Filter log messages using regex
-    """
+def filter_datum(fields: typing.List, redaction: str, message: str, separator: str) -> str:
+    """Filter log messages using regex"""
     for field in fields:
         pattern = re.compile(rf"({re.escape(separator)}{field}=)[^{separator}]*")
         message = pattern.sub(fr"\1{redaction}", message)
