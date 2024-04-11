004dbda768c0e3257360d248799c484b34ca84ca
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-11 17:04:25 +0200

    Fix Incompatible return value type (got PooledMySQLConnection | MySQLConnectionAbstract, expected MySQLConnection)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,8 @@ import typing
 import logging
 
 import mysql.connector
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
 
 
 def filter_datum(fields: typing.List[str], redaction: str,
@@ -55,11 +57,11 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Get database connector"""
-    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+def get_db() -> PooledMySQLConnection | MySQLConnectionAbstract:
+    """Get mysql database connector"""
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "r")
     password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "")
     db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
 
     db = mysql.connector.connect(
