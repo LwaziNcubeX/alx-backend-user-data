4f37ecb67a42b26617de16f0cb4dc9520807a415
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-11 15:30:55 +0200

    update regex format and remove the re.compile()

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,6 @@ def filter_datum(fields: typing.List, redaction: str,
                  message: str, separator: str) -> str:
     """Filter log messages using regex"""
     for field in fields:
-        pattern = re.compile(
-            rf"({re.escape(separator)}{field}=)[^{separator}]*")
-        message = pattern.sub(fr"\1{redaction}", message)
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
