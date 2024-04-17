cfb0e3347279c05eaa2b34631f50f10550dd820b
Lwazi Ncube <lwazincubex@gmail.com>
2024-04-17 03:53:41 +0200

    update method require_auth to define which routes need auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,7 +6,7 @@ from typing import List, TypeVar
 from flask import request
 
 
-class Auth(object):
+class Auth:
     """
     Auth related functions and class
     """
@@ -15,8 +15,14 @@ class Auth(object):
         """
         Checks if the path is valid
         """
+        if path is None or not excluded_paths:
+            return True
 
-        return False
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
